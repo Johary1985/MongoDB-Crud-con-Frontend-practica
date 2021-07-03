9ec180d4bf9271dde6b9cb6c2e345116a93b0dab
Johary1985 <77841530+Johary1985@users.noreply.github.com>
2021-07-03 14:16:17 -0300

    Finalizado el CRUD usando Javascript, Finalizado el visual de la practica, ejercicio OK

--- controllers/alumnoController.js
+++ controllers/alumnoController.js
@@ -3,7 +3,7 @@ const Alumno = require('../model/Alumno') //Traemos nuestro modelo
 /* Definiendo Metodos */
 
 /* Mostrar */
-module.exports.mostrar =(req, res)=>{
+module.exports.mostrar = (req, res)=>{
     Alumno.find({}, (error, alumnos)=>{
         if (error) {
             return res.status(500).json({
@@ -11,5 +11,57 @@ module.exports.mostrar =(req, res)=>{
             })
         }
         return res.render('index', {alumnos: alumnos}) //Con esto devolvemos en el navegador nuestro index.ejs
+    })
+}
+
+/* Crear */
+module.exports.crear = (req, res)=> {
+    // console.log(req.body)
+    const alumno = new Alumno({
+        nombre: req.body.nombre,
+        edad: req.body.edad
+    })
+    alumno.save(function(error, alumno){
+        if(error){
+
+            return res.status(500).json({
+            message: 'Error al crear Alumnos'
+
+            })
+        }
+        res.redirect('/')
+    })
+}
+
+/* Editar */
+module.exports.editar = (req, res)=>{
+    const id = req.body.id_editar
+    const nombre = req.body.nombre_editar
+    const edad = req.body.edad_editar
+    Alumno.findByIdAndUpdate(id, {nombre, edad}, (error, alumno)=>{
+        if(error){
+
+            return res.status(500).json({
+            message: 'Error al Editar Alumnos'
+
+            })
+        }
+        res.redirect('/')
+    })
+}
+
+/* Borrar */
+module.exports.borrar = (req, res)=>{
+    const id = req.params.id
+    Alumno.findByIdAndRemove(id, (error, alumno)=>{
+        if(error){
+
+            return res.status(500).json({
+            message: 'Error al Borrar Alumnos'
+
+            })
+        }
+        res.redirect('/')
+
     })
 }
--- routes/alumnos.js
+++ routes/alumnos.js
@@ -4,8 +4,17 @@ const router = express.Router()
 /* Para llamar al controlador */
 const alumnoController = require('../controllers/alumnoController')
 
-/* GET */
+/* GET ALL */
 router.get('/', alumnoController.mostrar)
+
+/* CREAR */
+router.post('/crear', alumnoController.crear)
+
+/* EDITAR ALUMNOS */
+router.post('/editar', alumnoController.editar) //Usamos post para modificar los datos de nuestro Modal y con el controlador lo editamos
+
+/* BORRAR ALUMNOS */
+router.get('/borrar/:id', alumnoController.borrar) //Usamos GET para tomar los datos y mediante el controlador lo borramos
 
 
 
